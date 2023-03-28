c460941e108dcf8e166df17819f3cb38b3471e6c
Guilherme Ramos <guilherme.silvano@hotmail.com>
2023-03-28 05:50:35 -0300

    feat: tdd /data/repository

--- lib/layers/domain/usecases/get_car_by_color/get_car_by_color_usecase_imp.dart
+++ lib/layers/domain/usecases/get_car_by_color/get_car_by_color_usecase_imp.dart
@@ -1,12 +1,12 @@
 import 'package:cleanarc/layers/domain/entities/car_entity.dart';
+import 'package:cleanarc/layers/domain/repositories/get_car_by_color_repository.dart';
 import 'package:cleanarc/layers/domain/usecases/get_car_by_color/get_car_by_color_usecase.dart';
 
 class GetCarByColorUseCaseImp implements GetCarByColorUseCase {
+  final GetCarByColorRepository _getCarByColorRepository;
+  GetCarByColorUseCaseImp(this._getCarByColorRepository);
   @override
   CarEntity call(String color) {
-    if (color.contains("red")) {
-      return CarEntity(plate: "ABC1234", doorsQuantity: 4, value: 50.000);
-    }
-    return CarEntity(plate: "DEF5678", doorsQuantity: 2, value: 20.000);
+    return _getCarByColorRepository(color);
   }
 }
